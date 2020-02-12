b13f8c789cc0935ba1cd41f185ecf7e6e1f4784f
JeongSerin <53457254+JeongSerin@users.noreply.github.com>
2020-02-12 09:14:27 +0900

    Update and rename BAEKJOON_Main_B_G5_17471_게리맨더링_정세린-1.java to BAEKJOON_Main_B_G5_17471_게리맨더링_정세린.java

--- JSR_Repo/BAEKJOON_Main_B_G5_17471_게리맨더링_정세린.java
+++ JSR_Repo/BAEKJOON_Main_B_G5_17471_게리맨더링_정세린.java
@@ -1,3 +1,8 @@
+/*
+ * 13524KB
+ * 80ms
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -23,6 +28,7 @@ public class Main_B_G5_17471_게리맨더링_정세린 {
 		region = new int[N + 1];
 		visited = new boolean[N + 1];
 		min = 100 * N;
+		int reg0 = 0, reg1 = 0;
 		st = new StringTokenizer(br.readLine());
 
 		// 인구수 입력 및 총합
@@ -41,55 +47,44 @@ public class Main_B_G5_17471_게리맨더링_정세린 {
 		}
 
 		L: for (int i = 1; i < (1 << N); i++) {
-
+			
+			// 새로 조합 만들면 visited, subsum초기화
 			subsum = 0;
 			Arrays.fill(visited, false);
 
 			for (int r = 1; r < N + 1; r++) {
 				region[r] = (i >> (r - 1)) % 2;
+				if (region[r] == 0) reg0 = r;
+				else if (region[r] == 1) reg1 = r;
 			}
+			
+			// 각 구역 탐색
+			dfs(reg0, 0);
+			dfs(reg1, 1);
 
+			// visit확인 미방문 발견되면 다시 조합 만듦
 			for (int j = 1; j < N + 1; j++) {
-				if (region[j] == 1 && !visited[j]) {
-					// 1구역 탐색
-					dfs(j, 1);
-					break;
-				}
-			}
-			for (int j = 1; j < N + 1; j++) {
-				if (region[j] == 0 && !visited[j]) {
-					// 0구역 탐색
-					dfs(j, 0);
-					break;
-				}
-			}
-
-			// visit확인
-			for (int j = 1; j < N + 1; j++) {
-				// 미방문 발견되면 다시 조합 만들기
 				if (!visited[j]) {
 					continue L;
 				}
 			}
 
-			// 구역 나누기 성공하면 구역 합 구하고
+			// 구역 나누기 성공하면 각 구역합 구함
 			for (int k = 1; k < N + 1; k++) {
 				if (region[k] == 0) {
 					subsum += num[k];
 				}
 			}
+			subsum2 = total - subsum;
 
 			// 구역 차이 구함
-			subsum2 = total - subsum;
 			diff = Math.abs(subsum - subsum2);
-			if (diff < min)
-				min = diff;
+			min = Math.min(min, diff);
 
 		}
-		if (min == 100 * N)
-			min = -1;
+		// 구역 나누기 실패하면 -1
+		if (min == 100 * N) min = -1;
 		System.out.println(min);
-
 	}
 
 	private static void dfs(int v, int reg) {
