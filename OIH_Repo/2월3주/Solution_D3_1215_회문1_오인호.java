34687173343cc34f930f115a5576f5e582d9d1c5
kickrisk <50090276+kickrisk@users.noreply.github.com>
2020-02-14 10:36:10 +0900

    Create Solution_D3_1215_회문1_오인호.java

---
+++ OIH_Repo/2월3주/Solution_D3_1215_회문1_오인호.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+	static char[][] map = new char[8][8]; 
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for(int t=1; t<=10; t++) {
+			int N = Integer.parseInt(br.readLine());
+			boolean flag = true;
+			int cnt = 0;
+			for(int i=0;i<8;i++) {
+				String st = br.readLine();
+				for(int j=0;j<8;j++) {
+					map[i][j] = st.charAt(j);
+				}
+			}
+			for(int i=0; i<8; i++) {
+				for(int j=0; j<8; j++) {
+					if(j <= 8-N) {
+						flag = true;
+						for(int k=0; k<N/2; k++) {
+							if(map[i][j+k] != map[i][j+N-1-k]) {
+								flag = false; break;
+							}
+						}
+						if(flag) cnt++;
+					}
+					if(i <= 8-N) {
+						flag = true;
+						for(int k=0; k<N/2; k++) {
+							if(map[i+k][j] != map[i+N-1-k][j]) {
+								flag = false; break;
+							}
+						}
+						if(flag) cnt++;
+					}
+				}
+			}
+			System.out.println("#" + t + " " + cnt);
+		}
+	}
+
+}
