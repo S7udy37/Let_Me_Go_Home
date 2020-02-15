d498e43d16db5d70485a6a53d312059addf85799
cent10 <31956355+cent10@users.noreply.github.com>
2020-02-15 17:32:47 +0900

    Add files via upload

---
+++ PJ_Repo/2월3주/Main_B_7785_회사에있는사람_박진.java
@@ -0,0 +1,49 @@
+// Silver V - 7785 : 회사에 있는 사람
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+public class Main_B_7785_회사에있는사람_박진 {
+
+	static int n;
+	static String[] people;
+	static TreeMap<String, String> treemap = new TreeMap<String, String>();
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		n = Integer.parseInt(br.readLine());
+		people = new String[2];
+		
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			people[0] = st.nextToken();
+			people[1] = st.nextToken();
+//			System.out.println(people[i][0] + " / " + people[i][1]);
+			
+			if (people[1].equals("enter")) {	// enter
+				treemap.put(people[0], people[0]);
+			}
+			else {	// leave
+				treemap.remove(people[0]);
+			}
+		}
+		
+		int size = treemap.size();
+		Object[] set = treemap.keySet().toArray();
+		for(int i = size-1; i >= 0; i--) {
+			sb.append(set[i]).append("\n");
+		}
+		
+//		for(String key : treemap.keySet())
+//			sb.append(treemap.get(key)).append("\n");
+		
+		System.out.println(sb);
+	}
+
+}
