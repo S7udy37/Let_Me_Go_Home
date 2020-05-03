05ed7423b81f82c97e07adf4a70c5783b528b4da
cent10 <31956355+cent10@users.noreply.github.com>
2020-05-03 18:26:11 +0900

    Update Solution_D5_1227_미로2_박진.java

--- PJ_Repo/4월4주/Solution_D5_1227_미로2_박진.java
+++ PJ_Repo/4월4주/Solution_D5_1227_미로2_박진.java
@@ -1,4 +1,4 @@
-// D5 - 1227 : [S/W 문제해결 기본] 7일차 - 미로2
+// D4 - 1227 : [S/W 문제해결 기본] 7일차 - 미로2
 
 /*
  * BFS
@@ -8,7 +8,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Solution_D5_1227_미로2_박진 {
+public class Solution_D4_1227_미로2_박진 {
 
 	static class Point {
 		int i, j;
