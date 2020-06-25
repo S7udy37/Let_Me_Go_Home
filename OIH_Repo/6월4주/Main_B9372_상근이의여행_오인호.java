6d28504074462423df21192b9e2e902658b4cf4d
kickrisk <50090276+kickrisk@users.noreply.github.com>
2020-06-25 16:44:40 +0900

    Create Main_B9372_상근이의여행_오인호.java

---
+++ OIH_Repo/6월4주/Main_B9372_상근이의여행_오인호.java
@@ -0,0 +1,25 @@
+package com.acmicpc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_B9372_상근이의여행_오인호 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		int T = Integer.parseInt(st.nextToken());
+		for(int t=1; t<=T; t++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			for(int i=0; i<M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int A = Integer.parseInt(st.nextToken()) - 1;
+				int B = Integer.parseInt(st.nextToken()) - 1;
+			}
+			System.out.println(N-1);
+		}
+	}
+}
